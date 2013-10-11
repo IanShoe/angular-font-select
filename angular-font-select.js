b1cc5d1d641615375b49cfb7234dbb868a8af056
Ian Shoemaker <iss90g@gmail.com>
2013-10-11 16:00:25 -0400

    better css and some animation

--- angular-font-select.js
+++ angular-font-select.js
@@ -1,18 +1,18 @@
 angular.module('ui.bootstrap.font-group', []).
 run(["$templateCache", function($templateCache) {
 	$templateCache.put("template/font-group/font-group.html",
-		"<span class=\"dropdown\">" +
+		"<span class=\"dropdown font-dropdown\">" +
 			"<span class=\"dropdown-toggle\">" +
-				"<input class=\"font-group-input\" ng-style=\"{'font-family': font}\" ng-model=\"font\" readonly>" +
+				"<input type=\"text\" class=\"form-control font-group-input\" ng-style=\"{'font-family': font}\" ng-model=\"font\" readonly>" +
 				"<b class=\"font-group-caret caret\"></b>" +
 			"</span>" +
 			"<div class=\"font-group-options dropdown-menu\">" +
-				"<div class=\"font-group-list-item\" ng-repeat=\"fontOption in fonts\" ng-style=\"{'font-family': fontOption}\" ng-class=\"{'active': currentFont == fontOption}\" ng-click=\"selectFont(fontOption)\">{{fontOption}}</div>" +
+				"<div class=\"font-group-list-item\" ng-repeat=\"fontOption in fonts | orderBy:'toString()'\" ng-style=\"{'font-family': fontOption}\" ng-class=\"{'active': currentFont == fontOption}\" ng-click=\"selectFont(fontOption)\">{{fontOption}}</div>" +
 			"</div>" +
 		"</span>"
 		);
 }]).
-directive('fontGroup', ['$document', function ($document) {
+directive('fontGroup', [function () {
 	return {
 		restrict: 'EA',
 		scope: {
